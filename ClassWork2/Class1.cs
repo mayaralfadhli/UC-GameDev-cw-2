f275e32ff11909274065024feb03f5000a3801e5
mayaralfadhli <84667222+mayaralfadhli@users.noreply.github.com>
2022-06-29 02:52:11 +0300

    class work 2 code

---
+++ ClassWork2/Class1.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string heroName = "Ghost";
+        int heroPower = 85;
+        string villainName = "Price";
+        int villainPower = 75;
+        if (heroPower > villainPower)
+        {
+            Console.WriteLine("villain damged");
+        }
+
+        else if (heroPower == villainPower)
+        {
+            Console.WriteLine("oh..");
+        }
+        else if (heroPower < villainPower)
+        {
+            Console.WriteLine("I am damged");
+        }
+        else
+        {
+            Console.WriteLine("....");
+        }
+    
+    }
